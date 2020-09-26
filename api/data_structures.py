09545f57e54d50d858601fd5158a0d59a9b26744
Jão <joao_bernrdi@outlook.com>
2020-09-26 16:58:34 -0300

    Rework with the API

--- api/data_structures.py
+++ api/data_structures.py
@@ -3,5 +3,6 @@
 class AttributeDict:
 	def __setitem__(self, key, value):
 		self.__setattr__(key.lower(), value)
+
 	def __getitem__(self, key):
 		return self.__getattribute__(key.lower())
--- api/models.py
+++ api/models.py
@@ -1,23 +1,26 @@
 # Data models that power the API
 from .data_structures import AttributeDict
+from api import API
 
 
 class Autor(AttributeDict):
-	def __init__(self, name, type):
+	def __init__(self, name: str, type: str, api: API):
 		self.name = name
 		self.type = type
+		self.__api = api
 
 	def __repr__(self):
 		return f"<Autor '{self.name}'>"
 
 
 class Proposta(AttributeDict):
-	def __init__(self, autor, ementa, ano, numero, keywords):
+	def __init__(self, autor: Autor, ementa: str, ano: str, numero: str, keywords: tuple, api: API):
 		self.autor = autor
 		self.ementa = ementa
 		self.ano = ano
 		self.numero = numero
 		self.keywords = keywords
+		self`.__api = api
 
 	def __repr__(self):
 		return f"<Proposta '{self.numero}/{self.ano}'>"
